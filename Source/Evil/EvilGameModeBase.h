b2829d3bcc9ecf68a7ceb03332d637d0b02f4f03
Mark Agne <magnenator@gmail.com>
2020-09-01 17:24:00 -0500

    empty C++ project created with UE 4.25.3

---
+++ Source/Evil/EvilGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EvilGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVIL_API AEvilGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
